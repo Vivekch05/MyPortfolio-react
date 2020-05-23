20ca0eef97a0c82eac70d2bb50d97dd367e253f9
Vivek Kumar Chaurasia <vivek.chaurasia@synapse.com>
2020-05-23 20:45:43 +0530

    Updates

---
+++ precache-manifest.16d0c2ebfdaa18b08d7e0dab4bc60147.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50591d6c0a13b0b9c1384a4a47aee3b1",
+    "url": "/MyPortfolio-react/index.html"
+  },
+  {
+    "revision": "221e1f2035075c84c1b1",
+    "url": "/MyPortfolio-react/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "608989c55bedb0567a1a",
+    "url": "/MyPortfolio-react/static/css/main.f0ae3aa3.chunk.css"
+  },
+  {
+    "revision": "221e1f2035075c84c1b1",
+    "url": "/MyPortfolio-react/static/js/2.1b960254.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MyPortfolio-react/static/js/2.1b960254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608989c55bedb0567a1a",
+    "url": "/MyPortfolio-react/static/js/main.b3ecef74.chunk.js"
+  },
+  {
+    "revision": "4232556ef02bede3afd7",
+    "url": "/MyPortfolio-react/static/js/runtime-main.cce93685.js"
+  },
+  {
+    "revision": "3b4e8dad669875ebbcecdae415eb1b00",
+    "url": "/MyPortfolio-react/static/media/Java.3b4e8dad.png"
+  },
+  {
+    "revision": "a279853789664c425ca4a8d50995e5ea",
+    "url": "/MyPortfolio-react/static/media/home.a2798537.jpg"
+  },
+  {
+    "revision": "d3a2ca90cde880e83247cfae597bf0f7",
+    "url": "/MyPortfolio-react/static/media/html.d3a2ca90.jpg"
+  },
+  {
+    "revision": "056e915690416b69db3f072931069e7e",
+    "url": "/MyPortfolio-react/static/media/react-native.056e9156.png"
+  },
+  {
+    "revision": "88b5e0ec0c5bdd59e7a1362add84bb57",
+    "url": "/MyPortfolio-react/static/media/react.88b5e0ec.png"
+  },
+  {
+    "revision": "c1a462d4193d5edf818f55d71f65befe",
+    "url": "/MyPortfolio-react/static/media/vivek.c1a462d4.jpg"
+  }
+]);
